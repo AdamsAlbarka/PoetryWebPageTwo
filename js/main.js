cbca599e5504de48da70c0e9b213deb6e1e7d514
AdamsAlbarka <adams.albarka@gmail.com>
2021-07-09 13:42:13 +0100

    This is the second commit of 9/07/2021

--- js/main.js
+++ js/main.js
@@ -13,6 +13,51 @@ const responsive = {
     }
 }
 
+// CAROUSELS
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 3000); // Change image every 3 seconds
+}
+// CAROUSELS END
+
+// QUOTES GENERATOR
+// QUOTES ENDS
+
+const quoteText = document.getElementById("quote-text"),
+      quoteTags = document.getElementById("quote-tags"),
+      quoteAuthor = document.getElementById("quote-author"),
+      genQuoteBtn = document.getElementById("gen-quote-btn");
+
+function quoteGen() {
+    fetch('https://api.quotable.io/random')
+    .then(response => response.json())
+    .then(data => {
+          quoteText.textContent = data.content;
+          quoteTags.textContent = data.tags;
+          quoteAuthor.textContent = `-- ${data.author}`;
+    })  
+}
+quoteGen();
+genQuoteBtn.addEventListener("click", ()=> {
+    quoteGen()
+})
+
+
 $(document).ready(function () {
 
     $nav = $('.nav');
